8b35d2c343e056364a5cac54dc62ca207e394a24
a2k-amit <amitkarlekar.sfdc@gmail.com>
2024-08-08 16:23:01 +0530

    changes made to recaptcha added

--- javascript.js
+++ javascript.js
@@ -1,11 +1,34 @@
-function handlebeforesubmit() {
-    let outputleaddate = document.querySelector('.output_lead_date');
-    let inputleaddate = document.querySelector('.input_lead_date');
-
-    console.log('input_lead_date.value', inputleaddate.value);
-
-    let formatedDate = new Date(inputleaddate.value).toLocaleDateString('en-IN');
-    outputleaddate.value = formatedDate;
+let captchaChecked = false;
+function isCaptchaTrue() {
+  captchaChecked = true;
 }
 
-function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
+function handlebeforesubmit(event) {
+  if (captchaChecked) {
+    let outputleaddate = document.querySelector(".output_lead_date");
+    let inputleaddate = document.querySelector(".input_lead_date");
+
+    console.log("input_lead_date.value", inputleaddate.value);
+
+    let formatedDate = new Date(inputleaddate.value).toLocaleDateString(
+      "en-IN"
+    );
+    outputleaddate.value = formatedDate;
+  } else {
+    alert("Please Check The Captcha");
+    event.preventDefault();
+  }
+}
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
