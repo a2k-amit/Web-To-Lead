813fa75e7202bc73926b547af3f26fa223b12ee6
a2k-amit <amitkarlekar.sfdc@gmail.com>
2024-08-08 16:22:42 +0530

    Recaptcha added to the web to lead form

--- javascript.js
+++ javascript.js
@@ -7,3 +7,5 @@ function handlebeforesubmit() {
     let formatedDate = new Date(inputleaddate.value).toLocaleDateString('en-IN');
     outputleaddate.value = formatedDate;
 }
+
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
